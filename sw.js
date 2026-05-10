af71adbfd80b693d3f5d7c55c9020b4907e4912c
dxfmotorsport <xabiportero08@gmail.com>
2026-05-10 20:17:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
-const VERSION = 'v1.0.10';
-const CACHE = 'dxf-' + VERSION;
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Unregister old service workers
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
