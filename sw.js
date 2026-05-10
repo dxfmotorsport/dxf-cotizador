6a07bea370c210aaab3342c716df9b6415ce4dd3
dxfmotorsport <xabiportero08@gmail.com>
2026-05-10 20:33:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-// Sin caché - siempre desde la red
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request.url + (e.request.url.includes('?') ? '&' : '?') + '_sw=' + Date.now()).catch(() => fetch(e.request)));
+  e.respondWith(fetch(e.request));
 });
